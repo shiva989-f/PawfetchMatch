c1bac083d30d3e9d95a011aec7f359de4411e80b
Shiva <kumarshiva05722@gmail.com>
2026-02-17 14:41:42 +0530

    Welcome fix

--- frontend/components/Welcome/Hero.jsx
+++ frontend/components/Welcome/Hero.jsx
@@ -65,6 +65,7 @@ const Hero = () => {
       split2.revert();
     };
   }, {});
+
   useGSAP(() => {
     const tl = gsap.timeline();
     tl.from(btnText.current, {
@@ -97,11 +98,13 @@ const Hero = () => {
           </div>
           <div>
             <p
+              ref={paraText1}
               className={`text-gray-600 text-center md:text-start ${alkatra.className} text-xl md:text-2xl`}
             >
               Adopt dogs, cats & rescued animal looking
             </p>
             <p
+              ref={paraText2}
               className={`text-gray-600 text-center md:text-start ${alkatra.className} text-xl md:text-2xl`}
             >
               for a loving home.
