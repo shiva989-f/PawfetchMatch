012dbf9932bad580bf3f4f56f30563ee4e5ef790
Shiva <kumarshiva05722@gmail.com>
2026-05-18 23:24:40 +0530

    Completed

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/Store/AuthStore";
-import { useEffect, useState, useRef } from "react";
+import { useEffect } from "react";
+
 import Navbar from "@/components/Welcome/Navbar";
 import Hero from "@/components/Welcome/Hero";
 import AdoptionRoadmap from "@/components/Welcome/AdoptionRoadmap";
@@ -10,13 +12,19 @@ import Footer from "@/components/Welcome/Footer";
 
 const page = () => {
   const router = useRouter();
-  const { isAuthenticated, user, isCheckingAuth } = useAuthStore();
+
+  const { isAuthenticated, user, isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
 
   useEffect(() => {
     if (!isCheckingAuth && isAuthenticated && user?.isVerified) {
       router.replace("/pets");
     }
   }, [isAuthenticated, user, isCheckingAuth]);
+
   return (
     <div className="font-sans bg-[#FFFDF9] text-[#1a1a1a] min-h-screen">
       <Navbar />
