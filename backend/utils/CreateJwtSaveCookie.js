df16dd1c6c073efea2de4ba4458f0c30f7b583b8
Shiva <kumarshiva05722@gmail.com>
2026-05-19 09:26:32 +0530

    Completed

--- backend/utils/CreateJwtSaveCookie.js
+++ backend/utils/CreateJwtSaveCookie.js
@@ -10,8 +10,7 @@ export const createJwtSaveInCookies = (res, userId, userRole) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "None", // Production env
-    // sameSite: "Lax", // Development env
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
